fce689d8e9c6405f509f941af4d3fb5e543817ae
CristinaGVO <vargasosoriocristina@gmail.com>
2025-12-05 08:12:20 -0800

    Add information to this proyect

--- models/clothes.js
+++ models/clothes.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
 const clotheSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: String,
+  name: { type: String, required: true },       // Nombre de la prenda
+  category: { type: String, required: true },   // Camiseta, pantalón, vestido, etc.
+  size: { type: String },                       // S, M, L...
+  color: { type: String },
+  price: { type: Number, min: 0 },
+  inStock: { type: Boolean, default: true },    // Disponible o no
+  image: String,                                // URL de imagen
+  description: String                           // Descripción de la prenda
 });
 
-const Clothe = mongoose.model('Clothe', clotheSchema);
+const Clothe = mongoose.model('Clothe', clotheSchema); // cree el modelo en mongoose
 module.exports = Clothe;
--- server.js
+++ server.js
@@ -10,29 +10,37 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
-// importando el modelo de clothes
+// importando el modelo de clothe para crear, leer,actualizar y eliminar
 
 const Clothe = require('./models/clothes.js');
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
+});
 
- 
 
 
 //conection de string
 
 
-  //Rutas
-// GET /
+//Rutas
+
+// GET 
+
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
+    res.render("index.ejs");
+});
+
+// server.js
+
+// GET /fruits/new
+app.get("/clothes/new", (req, res) => {
+  res.send("This route sends the user a form page!");
 });
 
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+    console.log('Listening on port 3000');
 });
 
